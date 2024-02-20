a947b8e5542b5a4da43d3d85f740d2c0269a8b82
Lyjia <tom.corelis@gmail.com>
2024-02-19 16:33:21 -0800

    fix not all keyname variants being recognized. Update to version 0.2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MP3TagRebuilder
 
-#### Version 0.1
+#### Version 0.2
 
 MP3TagRebuilder is a python script designed to fix a very specific type of corrupted MP3 ID3 tag that seems to appear rather often with purchases from Beatport.
 
@@ -49,7 +49,7 @@ You can pass mp3-tag-rebuilder a path to a folder, instead of mp3 files. It will
 
 ## Note About Tracks Already Imported to Serato
 
-Serato seems to cache ID3 tags to an internal database and seems to use that to accelerate track display in the file browser. It will not re-read ID3 tags until a given track is selected and cued, and it doesn't seem to update its internal database with this information.
+Serato seems to cache ID3 tags to an internal database and seems to use that to accelerate track display in the file browser. It will not re-read ID3 tags until a given track is selected and cued, and it doesn't seem to update its internal database with this information if it doesn't appear to have changed.
 
 Rather frustratingly, the data it caches seems to also carry this corruption and it will ignore new ID3 data even if you re-run analysis, which will make it seem as if this script does nothing. 
 
@@ -61,6 +61,8 @@ To do this workaround:
 1. Rename the file on your filesystem (from something like 'mysong.mp3' to 'mysong2.mp3', then cue it up in Serato. This will cause Serato to think the file is missing, and it deletes its cache for that file. The file will display as yellow with just its filename in Serato's file browser. 
 1. Then, rename the file back to its original filename and cue it up again. Serato will perform its analysis and save the updated ID3 tags to its cache.
 1. Subsequent uses of the given track will behave as expected, with the track preview appearing instantly and all metadata being correct.
+
+If you can think of a better way to do this, please let me know!
 
 ## License:
 
--- mp3-tag-rebuilder.py
+++ mp3-tag-rebuilder.py
@@ -1,36 +1,38 @@
 import argparse, os, glob
-from mutagen.id3 import ID3, error, ID3NoHeaderError
+from mutagen.id3 import ID3, error, ID3NoHeaderError, COMM, TKEY, UFID
 from mutagen.mp3 import MP3
 
-VERSION = '0.1'
+VERSION = '0.2'
 
 medieval_system = {
-    '1A': 'Abm',
-    '1B': 'Bmaj',
-    '2A': 'Ebm',
-    '2B': 'F#maj',
-    '3A': 'Bbm',
-    '3B': 'Dbmaj',
-    '4A': 'Fm',
-    '4B': 'Abmaj',
-    '5A': 'Cm',
-    '5B': 'Ebmaj',
-    '6A': 'Gm',
-    '6B': 'Bbmaj',
-    '7A': 'Dm',
-    '7B': 'Fmaj',
-    '8A': 'Am',
-    '8B': 'Cmaj',
-    '9A': 'Em',
-    '9B': 'Gmaj',
-    '10A': 'Bm',
-    '10B': 'Dmaj',
-    '11A': 'F#m',
-    '11B': 'Amaj',
-    '12A': 'C#m',
-    '12B': 'Emaj'
+    '1A': ['Abmin', 'G#min', 'Abm', 'G#m'],
+    '1B': ['Bmaj'],
+    '2A': ['Ebmin', 'D#min', 'Ebm', 'D#m'],
+    '2B': ['F#maj', 'Gbmaj'],
+    '3A': ['Bbmin', 'A#min', 'Bbm', 'A#m'],
+    '3B': ['Dbmaj', 'C#maj'],
+    '4A': ['Fmin', 'Fm'],
+    '4B': ['Abmaj', 'G#maj'],
+    '5A': ['Cmin', 'Cm'],
+    '5B': ['Ebmaj', 'D#maj'],
+    '6A': ['Gmin', 'Gm'],
+    '6B': ['Bbmaj', 'A#maj'],
+    '7A': ['Dmin', "Dm"],
+    '7B': ['Fmaj'],
+    '8A': ['Amin', 'Am'],
+    '8B': ['Cmaj'],
+    '9A': ['Emin', 'Em'],
+    '9B': ['Gmaj'],
+    '10A': ['Bmin', 'Bm'],
+    '10B': ['Dmaj'],
+    '11A': ['F#min', 'Gbmin', 'F#m', 'Gbm'],
+    '11B': ['Amaj'],
+    '12A': ['C#min', 'Dbmin', 'C#m', 'Dbm'],
+    '12B': ['Emaj']
 }
-medieval_system_inverse = {v: k for k, v in medieval_system.items()}
+
+# Invert the dictionary and flatten the list of equivalent keys
+medieval_system_inverse = {k2: k for k, v in medieval_system.items() for k2 in v}
 
 
 def log(msg):
@@ -46,10 +48,10 @@ def get_mp3_files(path):
         return mp3_files
     else:
         print(f"Path {path} does not exist!")
-        exit -1
+        exit(-1)
 
 
-def rebuild_id3_tags(file_path, convert_keys=False, strip_ufid=False):
+def rebuild_id3_tags(file_path, convert_key=False, strip_ufid=False):
     try:
         audio = MP3(file_path, ID3=ID3)
         log(f"Processing file {file_path}...")
@@ -60,14 +62,16 @@ def rebuild_id3_tags(file_path, convert_keys=False, strip_ufid=False):
     new_tags = ID3()
 
     for tag, value in audio.tags.items():
-        if convert_keys:
-            if tag == 'TKEY' and value.text[0] in medieval_system_inverse:
+        if convert_key and tag == 'TKEY':
+            if value.text[0] in medieval_system_inverse:
                 newval = medieval_system_inverse[value.text[0]]
                 log(f"Converting {tag}: {value.text[0]} --> {newval}...")
                 value.text = [newval]
+            else:
+                log(f"Key {value.text[0]} not recognized!")
 
         if strip_ufid:
-            if tag == "UFID":
+            if tag == 'UFID':
                 log(f"Stripping {tag}...")
                 continue
 
@@ -87,7 +91,7 @@ def rebuild_id3_tags(file_path, convert_keys=False, strip_ufid=False):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=f"Rebuild ID3 tags of an MP3 file. (Version {VERSION})")
     parser.add_argument('file_path', type=str, help="The path to the MP3 file, or a folder of mp3 files.")
-    parser.add_argument('--convert-keys', '-c', action='store_true', help="Convert abbreviated musical keys (e.g. "
+    parser.add_argument('--convert-key', '-c', action='store_true', help="Convert abbreviated musical keys (e.g. "
                                                                           "'Fmaj') to a certain medieval-themed notation system (e.g. '7B').")
     parser.add_argument('--strip-ufid', '-s', action='store_true', help="Strip tags from Beatport containing "
                                                                        "identifying information.")
@@ -97,5 +101,5 @@ if __name__ == "__main__":
 
     for file_path in all_files:
         rebuild_id3_tags(file_path,
-                         convert_keys=args.convert_keys,
+                         convert_key=args.convert_key,
                          strip_ufid=args.strip_ufid)
